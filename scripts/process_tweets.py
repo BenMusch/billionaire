b5c0e60e71b13a4007d1195ec07391cb7530afc3
Ben Muschol <benmuschol@gmail.com>
2020-01-21 21:10:39 -0800

    Use tweets as the data source (#2)

---
+++ scripts/process_tweets.py
@@ -0,0 +1,30 @@
+import json
+import re
+import sys
+
+HASHTAG_REGEX = r"#\w*[0-9a-zA-Z]+\w*[0-9a-zA-Z]\s?"
+QUOTE_REGEX = r"(.+)\-\s*\S+"
+USERNAME = sys.argv[1]
+
+def should_process(tweet):
+    if tweet['is_reply_to']: return False
+    if tweet['screen_name'] != USERNAME: return False
+
+    return True
+
+if __name__ == '__main__':
+    path = 'data/tweets_%s.json' % USERNAME 
+    with open(path) as f:
+        data = json.load(f)
+    data = filter(should_process, data)
+    final_tweets = []
+    for tweet in data:
+        text = tweet['text'].replace("\n", "")
+        no_hashtags = re.sub(HASHTAG_REGEX, '', text)
+        match = re.search(QUOTE_REGEX, no_hashtags)
+        if match:
+            final_tweets.insert(-1, match.group(1))
+        else:
+            final_tweets.insert(-1, no_hashtags)
+    with open("data/tweets.txt", "a") as f:
+        f.write("\n".join(final_tweets))
