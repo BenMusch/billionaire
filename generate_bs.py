c9bfb6087a792677676c57ef9a74cf124ca3f06c
Ben Muschol <ben.muschol@airbnb.com>
2020-01-22 17:23:01 -0800

    More training

--- generate_bs.py
+++ generate_bs.py
@@ -1,6 +1,6 @@
 from textgenrnn import textgenrnn
 
 textgen = textgenrnn()
-textgen.train_from_file(file_path='data/tweets.txt', num_epochs=1)
+textgen.train_from_file(file_path='data/tweets.txt', num_epochs=5)
 for _ in range(100):
     print(textgen.generate())
