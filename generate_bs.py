ce96030446438854ae4188036200df76a10d96d2
Ben Muschol <ben.muschol@airbnb.com>
2020-01-22 17:25:19 -0800

    Another change

--- generate_bs.py
+++ generate_bs.py
@@ -1,6 +1,6 @@
 from textgenrnn import textgenrnn
 
-textgen = textgenrnn()
+textgen = textgenrnn('textgenrnn_weights.hdf5')
 textgen.train_from_file(file_path='data/tweets.txt', num_epochs=5)
 for _ in range(100):
     print(textgen.generate())
